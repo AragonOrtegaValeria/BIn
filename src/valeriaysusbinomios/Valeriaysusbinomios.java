04f6478a58339665af51e04ad4944116bee419f0
AragonOrtegaValeria <30886660+AragonOrtegaValeria@users.noreply.github.com>
2017-08-23 22:57:40 -0500

    Update Valeriaysusbinomios.java

--- src/valeriaysusbinomios/Valeriaysusbinomios.java
+++ src/valeriaysusbinomios/Valeriaysusbinomios.java
@@ -12,7 +12,7 @@ public class Valeriaysusbinomios {
      */
     public static void main(String[] args) {
         ventana1 plis = new ventana1(); 
-        plis.llamame();
+        plis.llamame(); //llama al metodo que al mismo tiempo llama a los metodos de la otra clase
     }
     
 }
