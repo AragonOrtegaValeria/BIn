0ed1b9d821e54bdf44a91929fdba88b0780cd23d
AragonOrtegaValeria <30886660+AragonOrtegaValeria@users.noreply.github.com>
2017-08-23 22:56:09 -0500

    Update ventana1.java

--- src/valeriaysusbinomios/ventana1.java
+++ src/valeriaysusbinomios/ventana1.java
@@ -6,20 +6,20 @@ import javax.swing.*;
 
 public class ventana1 extends JFrame implements ActionListener  {
 
-JFrame framito; 
+JFrame framito; //declara componentes de la ventana
 JButton bot1, bot2;
 JLabel labela, labelb, labelres;
 JTextField numa, numb, res; 
-private int resultado = 0, calca, calab, calcb;  
+private int resultado = 0, calca, calab, calcb;  //declara variables
   
-    
+    //metodo para llamar a los otros.
 public void llamame(){
     ventana();
     elementos();
 
 }
 
-public void ventana(){
+public void ventana(){ //Crea la ventana con sus caracteristicas
     
     setLayout(null);
     setSize(700, 700);
@@ -27,9 +27,9 @@ public void ventana(){
     setDefaultCloseOperation(EXIT_ON_CLOSE);
    
 } 
-public void elementos(){   
+public void elementos(){   //Crea los elementos de la ventana
 
-    bot1 = new JButton("(a-b)^2");
+    bot1 = new JButton("(a-b)^2"); //crea un boton con el texto que va a mostrar, las medidas, lo agrega a la ventana y le agrega un Action Listener.
     bot1.setBounds(200, 150 , 100, 50);
     add(bot1);
     bot1.addActionListener(this); 
@@ -39,11 +39,11 @@ public void elementos(){
     add(bot2);
     bot2.addActionListener(this); 
     
-    numa = new JTextField();
+    numa = new JTextField(); //Crea campo de texto, agrega las medidas y lo agrega a la ventana
     numa.setBounds(200, 50, 50, 20);
     add(numa); 
     
-    labela = new JLabel("Coeficiente de x");
+    labela = new JLabel("Coeficiente de x"); //Crea un titulo para el campo de texto, da medidas y lo agrega
     labela.setBounds(250, 50, 200, 20);
     add(labela);
     
@@ -66,18 +66,18 @@ public void elementos(){
     setVisible(true);
 }
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) { //en caso de que se active
                 if(e.getSource().equals(bot2)){
-                    System.out.println("k");
-                    calca = Integer.parseInt(numa.getText());
-                    calcb = Integer.parseInt(numb.getText());
-                    calab = 2*calca*calcb;
+                    System.out.println("k"); 
+                    calca = Integer.parseInt(numa.getText()); 
+                    calcb = Integer.parseInt(numb.getText()); //Convierte cadena y devuelve un entero.
+                    calab = 2*calca*calcb; //calab, calca y calcb realizan las operaciones
                     calca = (calca)*(calca);
                     calcb = (calcb)*(calcb);
-                    res.setText(calca+"x^2 +"+calab+"xy +"+calcb+"y^2");
+                    res.setText(calca+"x^2 +"+calab+"xy +"+calcb+"y^2"); //devuelve el resultado
         }
 
-            if(e.getSource().equals(bot1)){
+            if(e.getSource().equals(bot1)){ //lo mismo pero cambia suma por resta
                 System.out.println("2");
                 calca = Integer.parseInt(numa.getText());
                 calcb = Integer.parseInt(numb.getText());
